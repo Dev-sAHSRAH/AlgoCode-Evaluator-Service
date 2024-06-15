3cb95678dd1950f02dd4930d049c83f47eb29ace
harsha <c.harsha0909@gmail.com>
2024-06-15 20:05:00 +0530

    Added: Bullboard UI

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 
+import serverAdapter from "./config/bullBoard.config";
 import serverConfig from "./config/server.config";
 import sampleProducer from "./producers/sampleProducer";
 import apiRouter from "./routes";
@@ -8,14 +9,32 @@ import sampleWorker from "./workers/sampleWorker";
 const app: Express = express();
 
 app.use("/api", apiRouter);
+app.use("/admin/queues", serverAdapter.getRouter());
 
 app.listen(serverConfig.PORT, () => {
   console.log(`Server started at PORT : ${serverConfig.PORT} 🔥`);
+  console.log(`BullBoard is running at ${serverConfig.PORT}/admin/queues`);
   sampleWorker("SampleQueue");
-  sampleProducer("SampleJob", {
-    name: "Harsha",
-    company: "Microsoft",
-    position: "SRE",
-    location: "HYD",
-  });
+
+  sampleProducer(
+    "SampleJob",
+    {
+      name: "Harsha",
+      company: "Microsoft",
+      position: "SRE",
+      location: "HYD",
+    },
+    2
+  );
+
+  sampleProducer(
+    "SampleJob",
+    {
+      name: "Niki",
+      company: "Microsoft",
+      position: "SRE",
+      location: "HYD",
+    },
+    1
+  );
 });
